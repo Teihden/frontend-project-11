d9d71366eec150ed89d133d8ad55dccc8af746fb
Teihden <teydev@gmail.com>
2023-08-15 00:21:27 +0500

    Add some refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 import path from 'path';
 import url from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const filename = url.fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 const config = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/js/index.js',
+  entry: {
+    index: './src/js/index.js',
+  },
   output: {
     clean: true,
     path: path.resolve(dirname, 'dist'),
-    filename: 'index.js',
+    filename: '[name].js',
   },
   devServer: {
     open: false,
@@ -25,16 +28,19 @@ const config = {
       scriptLoading: 'module',
       favicon: './src/assets/favicon.ico',
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.(eot|ttf|otf|woff2?|png|jpe?g|gif|webp|avif|svg)$/i,
